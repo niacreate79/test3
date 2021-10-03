33b6babac5a3c0c9bc7ff1f250476531cf0f34c1
Ilya Nechaev <nechaev_ia@mail.ru>
2021-10-03 12:09:19 +0300

    Second IDE commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi3");
         System.out.println("Hi_new");
+        System.out.println("Hi_new2");
+
+
     }
 }
